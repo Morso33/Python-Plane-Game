831276b5832904da4ffa350647d1029739cd797b
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-03-09 17:52:34 +0200

    fix

--- text_warp.py
+++ text_warp.py
@@ -1,7 +1,12 @@
 import textwrap
 
 def split_text(text, max_length):
-    return textwrap.wrap(text, width=max_length, break_long_words=False, replace_whitespace=False)
+    lines = text.split('\n')
+    wrapped_lines = []
+    for line in lines:
+        wrapped_lines.extend(
+            textwrap.wrap(line, width=max_length, break_long_words=False, replace_whitespace=False) or [''])
+    return wrapped_lines
 
 text = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")
 result = split_text(text, 30)
