4cff91fd3502202ddf9a0f4bf5d246a65f1972d3
Morso <69962221+Morso33@users.noreply.github.com>
2025-03-10 21:49:27 +0200

    Customer logic changes, payouts.

--- src/aircraft.py
+++ src/aircraft.py
@@ -1,3 +1,5 @@
+import random
+
 selected_aircraft = "Cessna 208 Caravan"
 
 def get_selected_aircraft():
@@ -24,3 +26,35 @@ def purchase_aircraft(con, aircraft):
     cur = con.cursor()
     query = "UPDATE aircraft SET owned = 1 WHERE name = ?"
     cur.execute(query, (aircraft,))
+
+def get_fuel_burn_per_km(con, aircraft):
+    cur = con.cursor()
+    query = "SELECT fuel_consumption_lph FROM aircraft WHERE name = ?"
+    cur.execute(query, (aircraft,))
+    result = cur.fetchone()
+    return result[0]
+
+def get_aircraft_type(con, aircraft):
+    cur = con.cursor()
+    query = "SELECT category FROM aircraft WHERE name = ?"
+    cur.execute(query, (aircraft,))
+    result = cur.fetchone()
+    return result[0]
+
+def get_payout(distance, aircraft_fuel_burn_per_km, aircraft_type):
+    costs = distance * aircraft_fuel_burn_per_km
+    payout = 0
+    if aircraft_type == "Small":
+        payout = costs * 0.25
+    elif aircraft_type == "Medium":
+        payout = costs * 3.5
+    elif aircraft_type == "Large":
+        payout = costs * 10
+    else:
+        pass
+
+    payout = payout / 10
+
+    random_factor = random.uniform(0.6, 1.6)
+    rounded = round(payout * random_factor, -2)
+    return int(rounded)
