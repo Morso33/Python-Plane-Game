a0894ba2a90d7477c04f6d343fafa8feca329e86
niskala-txk25k1 <Daniel.Niskala@metropolia.fi>
2025-03-12 01:30:26 +0200

    Delete old files

--- src/old/aircraft.py
+++
@@ -1,9 +0,0 @@
-#A class for all aircraft.
-#Data: aircraft_type, aircraft_name, aircraft_speed, aircraft_fuel_burn_per_km, aircraft_range
-class cAircraft:
-    def __init__(self, aircraft_type, aircraft_name, aircraft_speed, aircraft_fuel_burn_per_km, aircraft_range):
-        self.aircraft_type = aircraft_type
-        self.aircraft_name = aircraft_name
-        self.aircraft_speed = aircraft_speed
-        self.aircraft_fuel_burn_per_km = aircraft_fuel_burn_per_km
-        self.aircraft_range = aircraft_range
--- src/old/player.py
+++
@@ -1,5 +0,0 @@
-class cPlayer:
-    def __init__(self, current_airport_id, current_aircraft, money):
-        self.current_airport_id = current_airport_id
-        self.current_aircraft = current_aircraft
-        self.money = money
